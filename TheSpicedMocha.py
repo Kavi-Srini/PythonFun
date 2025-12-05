72db0fb8cfc70d0bd3cbba9168ccac5a226f0d01
Kavi-Srini <kavitha@Administrators-MacBook-Pro.local>
2025-12-04 21:10:30 -0500

    update

--- TheSpicedMocha.py
+++ TheSpicedMocha.py
@@ -1,10 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def greeting():
-    return "Welcome to The Spiced Mocha!"
 
-if __name__ == "__main__":
-    app.run()
+
+def index():
+    return render_template('index.html')
